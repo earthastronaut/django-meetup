c6091b1b13a9782a8c7f78e9dd873e0cd85af715
astrodsg_laptop <gregersen.dylan@gmail.com>
2014-09-15 11:49:24 -0600

    fixes so that setup.py can install

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 from distutils.core import setup
 import meetup
 
-readme = open('README.md').read()+open("LICENSE").read()
-packages = ['meetup']
 install_requires = open("requirements.txt").read().split("\n")
-ext_modules = []
+readme = open('README.rst').read()+open("LICENSE").read()
 
 setup(
     name="django-meetup",
     version=meetup.__version__,
-    py_modules=["meetup"],
+    packages=['meetup'],    
+    url="https://github.com/astrodsg/django-meetup",
     description="General purpose Django Meetup database to sync with Meetup.com.",
+    long_description=readme,
+    license="3-clause BSD style license",
     author="Dylan Gregersen",
     author_email = "gregersen.dylan@gmail.com",
-    license="3-clause BSD style license",
-    long_description=readme,
     platforms=["any"],
     classifiers=[
-        "Development Status :: 1 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
@@ -25,7 +23,5 @@ setup(
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    packages=packages,
-    ext_modules=ext_modules,
     install_requires=install_requires,    
 )
