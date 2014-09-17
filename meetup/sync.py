b9f5113cb3c91e3b60dbeb275e754bfde09f0d05
astrodsg_laptop <gregersen.dylan@gmail.com>
2014-09-16 23:53:58 -0600

    sync all event status objects

--- meetup/sync.py
+++ meetup/sync.py
@@ -12,7 +12,7 @@ DATE: Mon Sep 15 00:12:21 2014
 from __future__ import print_function, division, unicode_literals
 from django.conf import settings
 from meetup.api import MeetupClient
-from meetup.models import Venue, Group, Event
+from meetup.models import Venue, Group, Event, STATUS_OPTIONS
 
 MEETUP_KEY =  settings.MEETUP_KEY
 
@@ -31,9 +31,11 @@ def sync_group_events (group_id,client=None):
     
     # ======================= sync events for the group    
     for group_data in results:
+        # create group
         group = Group.objects.from_meetup_data(group_data)
         print(" -- for group {} --".format(group.name))
-        params['status'] = 'upcoming,past,cancelled'
+        # get all status options
+        params['status'] = ",".join(STATUS_OPTIONS) 
         events = client.invoke("/2/events",params)['results']
         for event_data in events:                    
             event = Event.objects.from_meetup_data(event_data)
