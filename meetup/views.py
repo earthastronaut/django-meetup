05d37a4eb4ceee0a819e5b7ec644784b5b4a25f6
astrodsg_laptop <gregersen.dylan@gmail.com>
2014-09-16 23:54:15 -0600

    view upcoming

--- meetup/views.py
+++ meetup/views.py
@@ -9,7 +9,7 @@ DATE: Tue Sep 16 08:40:49 2014
 
 from __future__ import print_function, division, unicode_literals
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
 from meetup.models import Venue,Group,Event
 from django.template import RequestContext
 
@@ -40,6 +40,22 @@ def next_group_event (group=MEETUP_GROUP_ID,**filter):
     n = len(events)
     if n > 0:
         return events[n-1]
+
+def view_upcoming_past_events (request):
+    context = RequestContext(request)
+    group_id = MEETUP_GROUP_ID
+    
+    group = Group.objects.get(pk=group_id)
+    
+    events = list(Event.objects.order_by("-event_timestamp").filter(status="upcoming",group=group_id))
+    events += list(Event.objects.order_by("-event_timestamp").filter(status="past",group=group_id))
+    venues = [e.venue.get() for e in events]
+    
+    context_dict = dict()
+    context_dict['events_venues'] = zip(events,venues)
+    context_dict['group'] = group
+    
+    return render_to_response("meetup/events.html",context_dict,context)
 
 def view_next_event (template):
     def render_view (request):
